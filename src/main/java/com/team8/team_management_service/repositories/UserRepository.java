9fe0535d8b2fec37f1013cde5779d838888a7c9b
alexey-skoblin <alexey-skoblin@gmail.com>
2024-10-18 01:35:55 +0400

    Rename package

--- src/main/java/com/team8/team_management_service/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.team8.team_management_service.repositories;
-
-import com.team8.team_management_service.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
