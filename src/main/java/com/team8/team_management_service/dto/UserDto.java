0b5c813f642abb9267544e696ddc87d9bf4cefd8
alexey-skoblin <alexey-skoblin@gmail.com>
2024-10-19 01:22:53 +0400

    Update user

---
+++ src/main/java/com/team8/team_management_service/dto/UserDto.java
@@ -0,0 +1,162 @@
+package com.team8.team_management_service.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * DTO for {@link com.team8.team_management_service.entity.User}
+ */
+public class UserDto implements Serializable {
+
+    private final Long id;
+    @Size(message = "Имя должно быть длиной от 2 до 256 символов", min = 2, max = 256)
+    @NotBlank
+    private final String name;
+    @Size(message = "Имя должно быть длиной от 2 до 256 символов", min = 2, max = 256)
+    @NotBlank
+    private final String username;
+    @Size(message = "Пароль должен быть длиной от 2 до 256 символов", min = 2, max = 256)
+    @NotBlank
+    private final String password;
+    @Size(message = "Фамилия должна быть длиной от 2 до 256 символов", min = 2, max = 256)
+    @NotBlank
+    private final String lastname;
+    @Size(message = "Должность должна быть длиной от 2 до 256 символов", min = 2, max = 256)
+    @NotBlank
+    private final String position;
+    @Size(message = "Опыт должен быть длиной от 2 до 2048 символов", min = 2, max = 2048)
+    @NotBlank
+    private final String experience;
+    @Size(message = "Опыт должен быть длиной от 2 до 2048 символов", min = 2, max = 2048)
+    @NotBlank
+    private final String messenger;
+    @Size(message = "Опыт должен быть длиной от 2 до 2048 символов", min = 2, max = 2048)
+    @NotBlank
+    private final int phoneNumber;
+    @Size(message = "Опыт должен быть длиной от 2 до 2048 символов", min = 2, max = 2048)
+    @NotBlank
+    private final String skills;
+    @Size(message = "Опыт должен быть длиной от 2 до 2048 символов", min = 2, max = 2048)
+    @NotBlank
+    private final String areaOfResponsibility;
+    @Size(message = "Опыт должен быть длиной от 2 до 2048 символов", min = 2, max = 2048)
+    @NotBlank
+    private final int age;
+    @Size(message = "Опыт должен быть длиной от 2 до 2048 символов", min = 2, max = 2048)
+    @NotBlank
+    private final boolean visibility;
+
+    public UserDto(Long id, String username, String password, String name, String lastname, String position, String experience, String messenger, int phoneNumber, String skills, String areaOfResponsibility, int age, boolean visibility) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.lastname = lastname;
+        this.position = position;
+        this.experience = experience;
+        this.messenger = messenger;
+        this.phoneNumber = phoneNumber;
+        this.skills = skills;
+        this.areaOfResponsibility = areaOfResponsibility;
+        this.age = age;
+        this.visibility = visibility;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public String getMessenger() {
+        return messenger;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public String getAreaOfResponsibility() {
+        return areaOfResponsibility;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean getVisibility() {
+        return visibility;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDto entity = (UserDto) o;
+        return Objects.equals(this.id, entity.id) &&
+                Objects.equals(this.username, entity.username) &&
+                Objects.equals(this.password, entity.password) &&
+                Objects.equals(this.name, entity.name) &&
+                Objects.equals(this.lastname, entity.lastname) &&
+                Objects.equals(this.position, entity.position) &&
+                Objects.equals(this.experience, entity.experience) &&
+                Objects.equals(this.messenger, entity.messenger) &&
+                Objects.equals(this.phoneNumber, entity.phoneNumber) &&
+                Objects.equals(this.skills, entity.skills) &&
+                Objects.equals(this.areaOfResponsibility, entity.areaOfResponsibility) &&
+                Objects.equals(this.age, entity.age) &&
+                Objects.equals(this.visibility, entity.visibility);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, name, lastname, position, experience, messenger, phoneNumber, skills, areaOfResponsibility, age, visibility);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "id = " + id + ", " +
+                "username = " + username + ", " +
+                "password = " + password + ", " +
+                "name = " + name + ", " +
+                "lastname = " + lastname + ", " +
+                "position = " + position + ", " +
+                "expirience = " + experience + ", " +
+                "messenger = " + messenger + ", " +
+                "phoneNumber = " + phoneNumber + ", " +
+                "skills = " + skills + ", " +
+                "areaOfResponsibility = " + areaOfResponsibility + ", " +
+                "age = " + age + ", " +
+                "visibility = " + visibility + ")";
+    }
+}
---
+++ src/main/java/com/team8/team_management_service/exception/CustomEntityNotFoundException.java
@@ -0,0 +1,25 @@
+package com.team8.team_management_service.exception;
+
+
+import com.team8.team_management_service.entity.User;
+import jakarta.persistence.EntityNotFoundException;
+
+public class CustomEntityNotFoundException extends EntityNotFoundException {
+
+    private final Class<?> entityClass;
+    private final Long id;
+
+    public CustomEntityNotFoundException(Class<?> tClass, Long id) {
+        super("Could not find " + tClass.getSimpleName() + " with id " + id);
+        this.entityClass = tClass;
+        this.id = id;
+    }
+
+    public Class<?> getEntityClass() {
+        return entityClass;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
