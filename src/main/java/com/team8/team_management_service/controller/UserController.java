db10c6963798cbf26723e0794b3860948cfdc818
alexey-skoblin <alexey.skoblin@gmail.com>
2024-10-21 23:20:57 +0400

    Update Dockerfile

--- src/main/java/com/team8/team_management_service/controller/UserController.java
+++ src/main/java/com/team8/team_management_service/controller/UserController.java
@@ -2,6 +2,7 @@ package com.team8.team_management_service.controller;
 
 import com.team8.team_management_service.dto.UserDto;
 import com.team8.team_management_service.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,34 +20,36 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<List<UserDto>> getAllUsers() {
-        List<UserDto> users = userService.findAll();
-        return ResponseEntity.ok(users);
+    public List<UserDto> getAllUsers() {
+        return userService.findAll();
     }
 
     @PostMapping
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
         UserDto createdUser = userService.create(userDto);
-        return ResponseEntity.ok(createdUser);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
-        UserDto user = userService.findById(id);
-        return ResponseEntity.ok(user);
+    public UserDto getUserById(@PathVariable Long id) {
+        return userService.findById(id);
     }
+
+    // Обновление пользователя по ID
+    @PutMapping("/{id}")
+    public UserDto updateUser(@RequestBody UserDto userDto, @PathVariable Long id) {
+        return userService.update(userDto, id);
+    }
+
+//    @PatchMapping("/{id}")
+//    public UserDto partialUpdateUser(@PathVariable Long id, @RequestBody UserDto userDto) {
+//        return userService.partialUpdate(id, userDto);
+//    }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.delete(id);
         return ResponseEntity.noContent().build();
-    }
-
-    // Обновление пользователя по ID
-    @PutMapping("/{id}")
-    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Long id) {
-        UserDto updatedUser = userService.update(userDto, id);
-        return ResponseEntity.ok(updatedUser);
     }
 
     // Поиск пользователя по юзернейму
